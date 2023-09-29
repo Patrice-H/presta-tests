764121276145a2b56f23e85e61612161905a3a75
Patrice-H <webmestre64@gmail.com>
2023-09-29 12:43:25 +0200

    Updates product variation

---
+++ var/cache/prod/smarty/cache/ps_newproducts/1/1/8/classic/38/ee/e0/38eee0cf19beb188c04556bca7128ad442b13f72.ps_newproductsviewstemplateshookps_newproducts.tpl.php
@@ -0,0 +1,521 @@
+<?php
+/* Smarty version 4.3.1, created on 2023-09-29 12:12:56
+  from 'module:ps_newproductsviewstemplateshookps_newproducts.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_6516a3281760d6_92231014',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '4a8df44403a47041b050fac755e17268c2a7c3e7' => 
+    array (
+      0 => 'module:ps_newproductsviewstemplateshookps_newproducts.tpl',
+      1 => 1678738694,
+      2 => 'module',
+    ),
+    '23fe90521d6617948f613f5cd689c0f58f568be9' => 
+    array (
+      0 => 'C:\\wamp64\\www\\presta-tests\\themes\\classic\\templates\\catalog\\_partials\\productlist.tpl',
+      1 => 1678738694,
+      2 => 'file',
+    ),
+    '42aa883d2cceea38b84bce098caee5c26a4d71f1' => 
+    array (
+      0 => 'C:\\wamp64\\www\\presta-tests\\themes\\classic\\templates\\catalog\\_partials\\miniatures\\product.tpl',
+      1 => 1678738694,
+      2 => 'file',
+    ),
+    '3e05db08128e9f065d2413d701c5094d6b54f0c0' => 
+    array (
+      0 => 'C:\\wamp64\\www\\presta-tests\\themes\\classic\\templates\\catalog\\_partials\\variant-links.tpl',
+      1 => 1678738694,
+      2 => 'file',
+    ),
+    '69bd1a55d2ef89bbe315b2101bde257a42e56abc' => 
+    array (
+      0 => 'C:\\wamp64\\www\\presta-tests\\themes\\classic\\templates\\catalog\\_partials\\product-flags.tpl',
+      1 => 1678738694,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 31536000,
+),true)) {
+function content_6516a3281760d6_92231014 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<section class="featured-products clearfix mt-3">
+  <h2 class="h2 products-section-title text-uppercase">
+    Nouveaux produits
+  </h2>
+  
+
+<div class="products">
+            
+<div class="js-product product col-xs-12 col-sm-6 col-lg-4 col-xl-3">
+  <article class="product-miniature js-product-miniature" data-id-product="9" data-id-product-attribute="75">
+    <div class="thumbnail-container">
+      <div class="thumbnail-top">
+        
+                      <a href="http://localhost/presta-tests/garcon/9-75-pantalon-chino.html#/15-colori-bleu_marine/29-taille_enfant-5_ans" class="thumbnail product-thumbnail">
+              <picture>
+                                                <img
+                  src="http://localhost/presta-tests/80-home_default/pantalon-chino.jpg"
+                  alt="Pantalon chino"
+                  loading="lazy"
+                  data-full-size-image-url="http://localhost/presta-tests/80-large_default/pantalon-chino.jpg"
+                  width="250"
+                  height="250"
+                />
+              </picture>
+            </a>
+                  
+
+        <div class="highlighted-informations">
+          
+            <a class="quick-view js-quick-view" href="#" data-link-action="quickview">
+              <i class="material-icons search">&#xE8B6;</i> Aperçu rapide
+            </a>
+          
+
+          
+                          <div class="variant-links">
+      <a href="http://localhost/presta-tests/garcon/9-76-pantalon-chino.html#/15-colori-bleu_marine/30-taille_enfant-6_ans"
+       class="color"
+       title="bleu marine"
+       aria-label="bleu marine"
+       style="background-color: #0a00b7"     ></a>
+      <a href="http://localhost/presta-tests/garcon/9-91-pantalon-chino.html#/16-colori-vert_kaki/29-taille_enfant-5_ans"
+       class="color"
+       title="vert kaki"
+       aria-label="vert kaki"
+       style="background-color: #436000"     ></a>
+      <a href="http://localhost/presta-tests/garcon/9-83-pantalon-chino.html#/26-colori-brique/29-taille_enfant-5_ans"
+       class="color"
+       title="brique"
+       aria-label="brique"
+       style="background-color: #db6700"     ></a>
+    <span class="js-count count"></span>
+</div>
+                      
+        </div>
+      </div>
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title"><a href="http://localhost/presta-tests/garcon/9-75-pantalon-chino.html#/15-colori-bleu_marine/29-taille_enfant-5_ans" content="http://localhost/presta-tests/garcon/9-75-pantalon-chino.html#/15-colori-bleu_marine/29-taille_enfant-5_ans">Pantalon chino</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="price" aria-label="Prix">
+                                                  14,99 €
+                              </span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+<div class="product-list-reviews" data-id="9" data-url="http://localhost/presta-tests/module/productcomments/CommentGrade">
+  <div class="grade-stars small-stars"></div>
+  <div class="comments-nb"></div>
+</div>
+
+        
+      </div>
+
+      
+    <ul class="product-flags js-product-flags">
+                    <li class="product-flag new">New</li>
+            </ul>
+
+    </div>
+  </article>
+</div>
+
+            
+<div class="js-product product col-xs-12 col-sm-6 col-lg-4 col-xl-3">
+  <article class="product-miniature js-product-miniature" data-id-product="4" data-id-product-attribute="53">
+    <div class="thumbnail-container">
+      <div class="thumbnail-top">
+        
+                      <a href="http://localhost/presta-tests/femme/4-53-tee-shirt-manches-courtes-col-en-v.html#/10-taille_adulte-xl/15-colori-bleu_marine" class="thumbnail product-thumbnail">
+              <picture>
+                                                <img
+                  src="http://localhost/presta-tests/30-home_default/tee-shirt-manches-courtes-col-en-v.jpg"
+                  alt="Tee-shirt manches courtes..."
+                  loading="lazy"
+                  data-full-size-image-url="http://localhost/presta-tests/30-large_default/tee-shirt-manches-courtes-col-en-v.jpg"
+                  width="250"
+                  height="250"
+                />
+              </picture>
+            </a>
+                  
+
+        <div class="highlighted-informations">
+          
+            <a class="quick-view js-quick-view" href="#" data-link-action="quickview">
+              <i class="material-icons search">&#xE8B6;</i> Aperçu rapide
+            </a>
+          
+
+          
+                          <div class="variant-links">
+      <a href="http://localhost/presta-tests/femme/4-61-tee-shirt-manches-courtes-col-en-v.html#/2-colori-noir/10-taille_adulte-xl"
+       class="color"
+       title="noir"
+       aria-label="noir"
+       style="background-color: #000000"     ></a>
+      <a href="http://localhost/presta-tests/femme/4-57-tee-shirt-manches-courtes-col-en-v.html#/3-colori-rouge/10-taille_adulte-xl"
+       class="color"
+       title="rouge"
+       aria-label="rouge"
+       style="background-color: #ff0000"     ></a>
+      <a href="http://localhost/presta-tests/femme/4-54-tee-shirt-manches-courtes-col-en-v.html#/11-taille_adulte-l/15-colori-bleu_marine"
+       class="color"
+       title="bleu marine"
+       aria-label="bleu marine"
+       style="background-color: #0a00b7"     ></a>
+    <span class="js-count count"></span>
+</div>
+                      
+        </div>
+      </div>
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title"><a href="http://localhost/presta-tests/femme/4-53-tee-shirt-manches-courtes-col-en-v.html#/10-taille_adulte-xl/15-colori-bleu_marine" content="http://localhost/presta-tests/femme/4-53-tee-shirt-manches-courtes-col-en-v.html#/10-taille_adulte-xl/15-colori-bleu_marine">Tee-shirt manches courtes...</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="price" aria-label="Prix">
+                                                  16,99 €
+                              </span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+<div class="product-list-reviews" data-id="4" data-url="http://localhost/presta-tests/module/productcomments/CommentGrade">
+  <div class="grade-stars small-stars"></div>
+  <div class="comments-nb"></div>
+</div>
+
+        
+      </div>
+
+      
+    <ul class="product-flags js-product-flags">
+                    <li class="product-flag new">New</li>
+            </ul>
+
+    </div>
+  </article>
+</div>
+
+            
+<div class="js-product product col-xs-12 col-sm-6 col-lg-4 col-xl-3">
+  <article class="product-miniature js-product-miniature" data-id-product="3" data-id-product-attribute="37">
+    <div class="thumbnail-container">
+      <div class="thumbnail-top">
+        
+                      <a href="http://localhost/presta-tests/femme/3-37-tee-shirt-manches-longues.html#/1-colori-blanc/10-taille_adulte-xl" class="thumbnail product-thumbnail">
+              <picture>
+                                                <img
+                  src="http://localhost/presta-tests/21-home_default/tee-shirt-manches-longues.jpg"
+                  alt="Tee-shirt manches longues"
+                  loading="lazy"
+                  data-full-size-image-url="http://localhost/presta-tests/21-large_default/tee-shirt-manches-longues.jpg"
+                  width="250"
+                  height="250"
+                />
+              </picture>
+            </a>
+                  
+
+        <div class="highlighted-informations">
+          
+            <a class="quick-view js-quick-view" href="#" data-link-action="quickview">
+              <i class="material-icons search">&#xE8B6;</i> Aperçu rapide
+            </a>
+          
+
+          
+                          <div class="variant-links">
+      <a href="http://localhost/presta-tests/femme/3-38-tee-shirt-manches-longues.html#/1-colori-blanc/11-taille_adulte-l"
+       class="color"
+       title="blanc"
+       aria-label="blanc"
+       style="background-color: #ffffff"     ></a>
+      <a href="http://localhost/presta-tests/femme/3-45-tee-shirt-manches-longues.html#/6-colori-rose_pale/10-taille_adulte-xl"
+       class="color"
+       title="rose pâle"
+       aria-label="rose pâle"
+       style="background-color: #ffecff"     ></a>
+      <a href="http://localhost/presta-tests/femme/3-41-tee-shirt-manches-longues.html#/10-taille_adulte-xl/15-colori-bleu_marine"
+       class="color"
+       title="bleu marine"
+       aria-label="bleu marine"
+       style="background-color: #0a00b7"     ></a>
+      <a href="http://localhost/presta-tests/femme/3-49-tee-shirt-manches-longues.html#/10-taille_adulte-xl/16-colori-vert_kaki"
+       class="color"
+       title="vert kaki"
+       aria-label="vert kaki"
+       style="background-color: #436000"     ></a>
+    <span class="js-count count"></span>
+</div>
+                      
+        </div>
+      </div>
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title"><a href="http://localhost/presta-tests/femme/3-37-tee-shirt-manches-longues.html#/1-colori-blanc/10-taille_adulte-xl" content="http://localhost/presta-tests/femme/3-37-tee-shirt-manches-longues.html#/1-colori-blanc/10-taille_adulte-xl">Tee-shirt manches longues</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="price" aria-label="Prix">
+                                                  16,99 €
+                              </span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+<div class="product-list-reviews" data-id="3" data-url="http://localhost/presta-tests/module/productcomments/CommentGrade">
+  <div class="grade-stars small-stars"></div>
+  <div class="comments-nb"></div>
+</div>
+
+        
+      </div>
+
+      
+    <ul class="product-flags js-product-flags">
+                    <li class="product-flag new">New</li>
+            </ul>
+
+    </div>
+  </article>
+</div>
+
+            
+<div class="js-product product col-xs-12 col-sm-6 col-lg-4 col-xl-3">
+  <article class="product-miniature js-product-miniature" data-id-product="2" data-id-product-attribute="21">
+    <div class="thumbnail-container">
+      <div class="thumbnail-top">
+        
+                      <a href="http://localhost/presta-tests/femme/2-21-debardeur-a-fines-bretelles.html#/1-colori-blanc/11-taille_adulte-l" class="thumbnail product-thumbnail">
+              <picture>
+                                                <img
+                  src="http://localhost/presta-tests/14-home_default/debardeur-a-fines-bretelles.jpg"
+                  alt="Débardeur à fines bretelles"
+                  loading="lazy"
+                  data-full-size-image-url="http://localhost/presta-tests/14-large_default/debardeur-a-fines-bretelles.jpg"
+                  width="250"
+                  height="250"
+                />
+              </picture>
+            </a>
+                  
+
+        <div class="highlighted-informations">
+          
+            <a class="quick-view js-quick-view" href="#" data-link-action="quickview">
+              <i class="material-icons search">&#xE8B6;</i> Aperçu rapide
+            </a>
+          
+
+          
+                          <div class="variant-links">
+      <a href="http://localhost/presta-tests/femme/2-22-debardeur-a-fines-bretelles.html#/1-colori-blanc/12-taille_adulte-m"
+       class="color"
+       title="blanc"
+       aria-label="blanc"
+       style="background-color: #ffffff"     ></a>
+      <a href="http://localhost/presta-tests/femme/2-29-debardeur-a-fines-bretelles.html#/2-colori-noir/11-taille_adulte-l"
+       class="color"
+       title="noir"
+       aria-label="noir"
+       style="background-color: #000000"     ></a>
+      <a href="http://localhost/presta-tests/femme/2-25-debardeur-a-fines-bretelles.html#/11-taille_adulte-l/15-colori-bleu_marine"
+       class="color"
+       title="bleu marine"
+       aria-label="bleu marine"
+       style="background-color: #0a00b7"     ></a>
+      <a href="http://localhost/presta-tests/femme/2-33-debardeur-a-fines-bretelles.html#/11-taille_adulte-l/19-colori-fushia"
+       class="color"
+       title="fushia"
+       aria-label="fushia"
+       style="background-color: #ff00ff"     ></a>
+    <span class="js-count count"></span>
+</div>
+                      
+        </div>
+      </div>
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title"><a href="http://localhost/presta-tests/femme/2-21-debardeur-a-fines-bretelles.html#/1-colori-blanc/11-taille_adulte-l" content="http://localhost/presta-tests/femme/2-21-debardeur-a-fines-bretelles.html#/1-colori-blanc/11-taille_adulte-l">Débardeur à fines bretelles</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="price" aria-label="Prix">
+                                                  19,99 €
+                              </span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+<div class="product-list-reviews" data-id="2" data-url="http://localhost/presta-tests/module/productcomments/CommentGrade">
+  <div class="grade-stars small-stars"></div>
+  <div class="comments-nb"></div>
+</div>
+
+        
+      </div>
+
+      
+    <ul class="product-flags js-product-flags">
+                    <li class="product-flag new">New</li>
+            </ul>
+
+    </div>
+  </article>
+</div>
+
+            
+<div class="js-product product col-xs-12 col-sm-6 col-lg-4 col-xl-3">
+  <article class="product-miniature js-product-miniature" data-id-product="1" data-id-product-attribute="5">
+    <div class="thumbnail-container">
+      <div class="thumbnail-top">
+        
+                      <a href="http://localhost/presta-tests/homme/1-5-pantalon-chino-coupe-regular.html#/1-colori-blanc/10-taille_adulte-xl" class="thumbnail product-thumbnail">
+              <picture>
+                                                <img
+                  src="http://localhost/presta-tests/2-home_default/pantalon-chino-coupe-regular.jpg"
+                  alt="Pantalon chino coupe regular"
+                  loading="lazy"
+                  data-full-size-image-url="http://localhost/presta-tests/2-large_default/pantalon-chino-coupe-regular.jpg"
+                  width="250"
+                  height="250"
+                />
+              </picture>
+            </a>
+                  
+
+        <div class="highlighted-informations">
+          
+            <a class="quick-view js-quick-view" href="#" data-link-action="quickview">
+              <i class="material-icons search">&#xE8B6;</i> Aperçu rapide
+            </a>
+          
+
+          
+                          <div class="variant-links">
+      <a href="http://localhost/presta-tests/homme/1-6-pantalon-chino-coupe-regular.html#/1-colori-blanc/11-taille_adulte-l"
+       class="color"
+       title="blanc"
+       aria-label="blanc"
+       style="background-color: #ffffff"     ></a>
+      <a href="http://localhost/presta-tests/homme/1-17-pantalon-chino-coupe-regular.html#/2-colori-noir/10-taille_adulte-xl"
+       class="color"
+       title="noir"
+       aria-label="noir"
+       style="background-color: #000000"     ></a>
+      <a href="http://localhost/presta-tests/homme/1-9-pantalon-chino-coupe-regular.html#/10-taille_adulte-xl/15-colori-bleu_marine"
+       class="color"
+       title="bleu marine"
+       aria-label="bleu marine"
+       style="background-color: #0a00b7"     ></a>
+      <a href="http://localhost/presta-tests/homme/1-13-pantalon-chino-coupe-regular.html#/10-taille_adulte-xl/16-colori-vert_kaki"
+       class="color"
+       title="vert kaki"
+       aria-label="vert kaki"
+       style="background-color: #436000"     ></a>
+    <span class="js-count count"></span>
+</div>
+                      
+        </div>
+      </div>
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title"><a href="http://localhost/presta-tests/homme/1-5-pantalon-chino-coupe-regular.html#/1-colori-blanc/10-taille_adulte-xl" content="http://localhost/presta-tests/homme/1-5-pantalon-chino-coupe-regular.html#/1-colori-blanc/10-taille_adulte-xl">Pantalon chino coupe regular</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="price" aria-label="Prix">
+                                                  39,99 €
+                              </span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+<div class="product-list-reviews" data-id="1" data-url="http://localhost/presta-tests/module/productcomments/CommentGrade">
+  <div class="grade-stars small-stars"></div>
+  <div class="comments-nb"></div>
+</div>
+
+        
+      </div>
+
+      
+    <ul class="product-flags js-product-flags">
+                    <li class="product-flag new">New</li>
+            </ul>
+
+    </div>
+  </article>
+</div>
+
+    </div>
+  <a class="all-product-link float-xs-left float-md-right h4" href="http://localhost/presta-tests/nouveaux-produits">
+    Tous les nouveaux produits<i class="material-icons">&#xE315;</i>
+  </a>
+</section>
+
+<?php }
+}
