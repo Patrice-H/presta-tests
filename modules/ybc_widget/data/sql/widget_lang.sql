ff2721703b2469e40a1b25c50b2f3c135379b3ec
Patrice-H <webmestre64@gmail.com>
2024-02-18 00:07:18 +0100

    ..

--- modules/ybc_widget/data/sql/widget_lang.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `_DB_PREFIX_ybc_widget_widget_lang` VALUES('73','_ID_LANG_','Luxury Earrings','','');
-INSERT INTO `_DB_PREFIX_ybc_widget_widget_lang` VALUES('74','_ID_LANG_','Women’s Rings','','');
-INSERT INTO `_DB_PREFIX_ybc_widget_widget_lang` VALUES('75','_ID_LANG_','Women’s Necklace','','');
-
-
